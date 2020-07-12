d040e0fc6587a4d869f8a12005d66eed7bd2e277
Nizzy Hernandez <3134183631y@gmail.com>
2020-07-11 19:51:28 -0500

    Se agregaron nuevos estilos

--- panel/content/start.php
+++ panel/content/start.php
@@ -1,8 +1,7 @@
-<div class="background-svg">
-    <br><br>
+<div class="container-navbar">
     <div class="container text-left">
         <div class="container jumbotron">
-            <h1 class="display-4">¡Bienvenido de nuevo!</h1>
+            <h1 class="display-4">¡Bienvenido #Usuario!</h1>
             <h4 class="lead">Las noticias más actuales acerca de SSNotes apareceran aquí.</h4>
             <hr class="my-4">
             <h4>¿Quieres ir a tu sección de notas?</h4>
