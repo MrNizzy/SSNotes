ba45bd8b045b0976de7ee21130b23e5443f928cf
Nizzy Hernandez <3134183631y@gmail.com>
2020-07-11 19:52:15 -0500

    Se actualizó las clases y contenido para bootstrap v4.5

--- panel/components/footer.php
+++ panel/components/footer.php
@@ -1,16 +1,17 @@
-<div class="bg-primary">
-    <div class="panel panel-success text-center">
-        <div class="panel-heading">
+<footer class="bg-primary">
+    <div class="card bg-success text-center" style="padding-top:20px">
+        <div class="card-heading">
         </div>
     </div>
     <div class="container mx-auto">
         <div class="row">
-            <div class="col-4 mx-auto text-center">
-                <img src="../static/img/logo.svg" alt="SSNotes" width="50px" class="mx-auto d-block">
-                <p class="text-muted text-center"><small><strong>SSNotes - Created by OverCode_<br>
+            <div class="mx-auto text-center">
+                <br>
+                <img src="../static/img/logo-lg.svg" alt="SSNotes" height="55px" class="mx-auto d-block">
+                <p class="text-light text-center"><small><strong>SSNotes - Created by OverCode_<br>
                             All rights reserved ©<br>
                             2020</strong></small></p>
             </div>
         </div>
     </div>
-</div>
+</footer>
--- panel/content/content-horario.php
+++ panel/content/content-horario.php
@@ -1,16 +1,41 @@
-<div class="background-svg">
-
-    <br><br>
+<div class="container-navbar">
     <div class="container">
-        <div class="alert alert-dismissible alert-info">
-            <button type="button" class="close" data-dismiss="alert">&times;</button>
-            <h4>Nota:</h4>
-            <p>Puedes acceder a la información de la hora, lugar de la asignatura pulsando encima de ella.</p>
+        <div class="card-deck">
+            <div class="card">
+                <img src="../static/img/book.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../static/img/book.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../static/img/book.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This card has even longer content than the first to show that equal height
+                        action.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
         </div>
-
-        <?php
-            require_once("components/horario-tabla.php")
-        ?>
-
     </div>
+    <br>
 </div>
