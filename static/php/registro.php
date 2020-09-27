f3cbf9cea95a3142bec9509704c70129c2bc6304
Nizzy Hernandez <3134183631y@gmail.com>
2020-09-26 20:13:34 -0500

    Funciones para conexión con la base de datos y registro de usuarios.

---
+++ static/php/registro.php
@@ -0,0 +1,44 @@
+<?php
+
+include("conexion.php");
+
+if (isset($_POST['registro'])){
+    if (strlen($_POST['usuario']) >= 1 && strlen($_POST['nombre']) >= 1 && strlen($_POST['email']) >= 1 && strlen($_POST['password']) >= 1 && strlen($_POST['repassword']) >= 1){
+
+        $usuario = trim($_POST['usuario']);
+        $nombre = trim($_POST['nombre']);
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
+        $repassword = trim($_POST['repassword']);
+        $fechareg = date("d/m/y");
+        
+        if(strlen($_POST['password']) == strlen($_POST['repassword'])){
+            
+
+            $consulta = "INSERT INTO Users (user, name, email, password, reg_date) VALUES ('$usuario', '$nombre', '$email', '$password', '$fechareg')";
+            $resultado = mysqli_query($conex, $consulta);
+
+            if ($resultado) {
+                ?>
+                <h3 class="alert alert-dismissible alert-success">¡Te haz registrado correctamente!</h3>
+                <?php
+            } else {
+                ?>
+                <h4 class="alert alert-dismissible alert-danger">¡El nombre de usuario o e-mail ya existe, por favor ingresa otra información!</h4>
+                <?php
+            }
+        }else{
+            ?>
+            <h4 class="alert alert-dismissible alert-warning">¡Las contraseñas no coinciden!</h4>
+            <?php
+        }
+
+        
+    } else {
+        ?>
+        <h3 class="alert alert-dismissible alert-info">¡Por favor llena los datos!</h3>
+        <?php
+    } 
+}
+
+?>
